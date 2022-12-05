fdf780fe6e659f6299c208feff01e0f68f597a90
lonebhen <ben.angmortey@gmail.com>
2022-12-05 10:03:13 +0000

    0

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,13 +1,7 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-#include <elf.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+int _putchar(char c);
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
